82891f1a3e69568813313cf2c5b3b60a0408a7ce
ryadh.afcepf@gmail.com <ryadh.afcepf@gmail.com@52cbff0f-ab16-f77c-8b75-df5b4e13430b>
2014-07-07 13:16:44 +0000

    MAJ des MBeans/Bussiness de Avis et Trajet

--- ProjetAtod21/CoVoiturage_Application/CoVoiturage_web/src/main/java/fr/afcepf/atod21/coVoiturage/managedBean/TrajetMBean.java
+++ ProjetAtod21/CoVoiturage_Application/CoVoiturage_web/src/main/java/fr/afcepf/atod21/coVoiturage/managedBean/TrajetMBean.java
@@ -5,11 +5,16 @@ import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.SessionScoped;
 
 import fr.afcepf.atod21.coVoiturage.business.IBusinessTrajet;
+import fr.afcepf.atod21.coVoiturage.entity.Trajet;
+import fr.afcepf.atod21.coVoiturage.entity.Utilisateur;
 
 @ManagedBean
 @SessionScoped
 public class TrajetMBean {
 
+	private Trajet trajet;
+	private Utilisateur utilisateur;
+	
 	@ManagedProperty (value="#{businessTrajetImpl}")
 	private IBusinessTrajet businessTrajet;
 
@@ -19,6 +24,22 @@ public class TrajetMBean {
 
 	public void setBusinessTrajet(IBusinessTrajet businessTrajet) {
 		this.businessTrajet = businessTrajet;
+	}
+
+	public Trajet getTrajet() {
+		return trajet;
+	}
+
+	public void setTrajet(Trajet trajet) {
+		this.trajet = trajet;
+	}
+
+	public Utilisateur getUtilisateur() {
+		return utilisateur;
+	}
+
+	public void setUtilisateur(Utilisateur utilisateur) {
+		this.utilisateur = utilisateur;
 	}
 	
 	
