96ab61319b65953aa498c6a12f8e9ebc9c56c490
ryadh.afcepf@gmail.com <ryadh.afcepf@gmail.com@52cbff0f-ab16-f77c-8b75-df5b4e13430b>
2014-08-18 16:22:15 +0000

    MAJ

--- trunk/ProjetAtod21/CoVoiturage_Application/CoVoiturage_data/src/main/java/fr/afcepf/atod21/coVoiturage/utils/Consts.java
+++ trunk/ProjetAtod21/CoVoiturage_Application/CoVoiturage_data/src/main/java/fr/afcepf/atod21/coVoiturage/utils/Consts.java
@@ -4,6 +4,7 @@ public final class Consts {
 
     public static final String EN_COURS = "en cours";
     public static final String PROPOSE = "proposé";
+    public static final String COMPLET = "complet";
     public static final String TERMINE = "terminé";
     public static final String CONDUCTEUR = "conducteur";
     public static final String PASSAGER = "passager";
