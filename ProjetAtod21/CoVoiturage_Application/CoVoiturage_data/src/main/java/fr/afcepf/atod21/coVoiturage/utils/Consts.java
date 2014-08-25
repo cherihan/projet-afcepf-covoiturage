d291517a97ce32ac9e6c10f4b102f6fc5ef455fc
ryadh.afcepf@gmail.com <ryadh.afcepf@gmail.com@52cbff0f-ab16-f77c-8b75-df5b4e13430b>
2014-08-25 06:49:16 +0000

    MAJ

--- ProjetAtod21/CoVoiturage_Application/CoVoiturage_data/src/main/java/fr/afcepf/atod21/coVoiturage/utils/Consts.java
+++ ProjetAtod21/CoVoiturage_Application/CoVoiturage_data/src/main/java/fr/afcepf/atod21/coVoiturage/utils/Consts.java
@@ -2,10 +2,12 @@ package fr.afcepf.atod21.coVoiturage.utils;
 
 public final class Consts {
 
+    public static final String EMPTY = "";
     public static final String EN_COURS = "en cours";
     public static final String PROPOSE = "proposé";
     public static final String COMPLET = "complet";
     public static final String TERMINE = "terminé";
+    public static final String ALL_TRAJETS = "tous les trajets";
     public static final String CONDUCTEUR = "conducteur";
     public static final String PASSAGER = "passager";
     
