076c33d21158a92fd173c09ad432ea2546ac1834
fou005@gmail.com <fou005@gmail.com@52cbff0f-ab16-f77c-8b75-df5b4e13430b>
2014-08-28 15:25:45 +0000

    modif TestRate

--- trunk/ProjetAtod21/CoVoiturage_Application/CoVoiturage_data/src/main/java/fr/afcepf/atod21/coVoiturage/daoImpl/DaoTrajetImpl.java
+++ trunk/ProjetAtod21/CoVoiturage_Application/CoVoiturage_data/src/main/java/fr/afcepf/atod21/coVoiturage/daoImpl/DaoTrajetImpl.java
@@ -19,167 +19,168 @@ import fr.afcepf.atod21.coVoiturage.utils.Consts;
 @Transactional
 public class DaoTrajetImpl implements IDaoTrajet {
 
-	@PersistenceContext
-	private EntityManager em;
+    @PersistenceContext
+    private EntityManager em;
 
-	@Override
-	public void ajouterCommentaireTrajet(int idTrajet, int idUser) {
-		// TODO Auto-generated method stub
+    @Override
+    public void ajouterCommentaireTrajet(int idTrajet, int idUser) {
+        // TODO Auto-generated method stub
 
-	}
+    }
 
-	@Override
-	public void supprimerTrajet(int idTrajet, int idUser) {
-		// TODO Auto-generated method stub
+    @Override
+    public void supprimerTrajet(int idTrajet, int idUser) {
+        // TODO Auto-generated method stub
 
-	}
+    }
 
-	@Override
-	public Integer creerTrajet(Trajet trajet, Utilisateur user) {
-	    
-		trajet.setVilleDepart(em.find(Ville.class, trajet.getVilleDepart().getIdVille()));
-		trajet.setVilleArrivee(em.find(Ville.class, trajet.getVilleArrivee().getIdVille()));
-		List<Utilisateur> listUsersHasTrajet = new ArrayList<Utilisateur>();
-		listUsersHasTrajet.add(user);
-		trajet.setUtilisateurs(listUsersHasTrajet);
-		em.persist(trajet);
-		user = em.find(Utilisateur.class, user.getIdUtilisateur());
-		user.getTrajets().add(trajet);
-		em.persist(user);
-		em.flush();
-		return trajet.getIdTrajet();
-	}
+    @Override
+    public Integer creerTrajet(Trajet trajet, Utilisateur user) {
 
-	   @Override
-	    public boolean sInscrireTrajet(Trajet trajet, Utilisateur user) {
-	        
-	        
-	        //user = em.find(Utilisateur.class, user.getIdUtilisateur());
+        trajet.setVilleDepart(em.find(Ville.class, trajet.getVilleDepart()
+                .getIdVille()));
+        trajet.setVilleArrivee(em.find(Ville.class, trajet.getVilleArrivee()
+                .getIdVille()));
+        List<Utilisateur> listUsersHasTrajet = new ArrayList<Utilisateur>();
+        listUsersHasTrajet.add(user);
+        trajet.setUtilisateurs(listUsersHasTrajet);
+        em.persist(trajet);
+        user = em.find(Utilisateur.class, user.getIdUtilisateur());
+        user.getTrajets().add(trajet);
+        em.persist(user);
+        em.flush();
+        return trajet.getIdTrajet();
+    }
 
-	        if (user != null){
-	        
-	            System.out.println("===================================================================");
-	            System.out.println("trajet à ajouter " + trajet.getIdTrajet());
-	            System.out.println("user à ajouter " + user.getIdUtilisateur());
-	            System.out.println("===================================================================");
-	            
-	        user.getTrajets().add(trajet);
-	            
-	        
-	        em.persist(user);
-	        return true; 
-	        }
-	        return false;
-	        
-	    }
+    @Override
+    public boolean sInscrireTrajet(Trajet trajet, Utilisateur user) {
 
-	    @Override
-	    public boolean seDesinscrireTrajet(Integer idTrajet, Integer idUser) {
-	        
-	        return false;
-	    }
+        // user = em.find(Utilisateur.class, user.getIdUtilisateur());
 
-	    @Override
-	    public boolean annulerInscriptionTrajet(Integer idTrajet, Integer idUser) {
-	        // TODO Auto-generated method stub
-	        return false;
-	    }
+        if (user != null) {
 
-	@Override
-	public List<Ville> getAllVilles() {
-		return em.createQuery("SELECT v FROM Ville v").getResultList();
-	}
+            user.getTrajets().add(trajet);
 
-	@Override
-	public List<Trajet> getAllHistoTrajets(int idUser) {
-		
-		String query = "SELECT u FROM Utilisateur u inner join fetch u.trajets WHERE u.idUtilisateur = :idUser";
-		Utilisateur u = (Utilisateur) em.createQuery(query).setParameter("idUser",idUser).getSingleResult();
-		System.out.println("===> taille = " + u.getTrajets().size());
-		return u.getTrajets();
-	}
+            em.persist(user);
+            return true;
+        }
+        return false;
 
-   @Override
-    public List<Trajet> getHistoTrajetsAsConductorByType(int idUser, String typeHistoTrajet) {
-        
+    }
+
+    @Override
+    public boolean seDesinscrireTrajet(Integer idTrajet, Integer idUser) {
+
+        return false;
+    }
+
+    @Override
+    public boolean annulerInscriptionTrajet(Integer idTrajet, Integer idUser) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public List<Ville> getAllVilles() {
+        return em.createQuery("SELECT v FROM Ville v").getResultList();
+    }
+
+    @Override
+    public List<Trajet> getAllHistoTrajets(int idUser) {
+
         String query = "SELECT u FROM Utilisateur u inner join fetch u.trajets WHERE u.idUtilisateur = :idUser";
-        Utilisateur u = (Utilisateur) em.createQuery(query).setParameter("idUser",idUser).getSingleResult();
+        Utilisateur u = (Utilisateur) em.createQuery(query)
+                .setParameter("idUser", idUser).getSingleResult();
+        System.out.println("===> taille = " + u.getTrajets().size());
+        return u.getTrajets();
+    }
+
+    @Override
+    public List<Trajet> getHistoTrajetsAsConductorByType(int idUser,
+            String typeHistoTrajet) {
+
+        String query = "SELECT u FROM Utilisateur u inner join fetch u.trajets WHERE u.idUtilisateur = :idUser";
+        Utilisateur u = (Utilisateur) em.createQuery(query)
+                .setParameter("idUser", idUser).getSingleResult();
 
         List<Trajet> listeAllTrajets = u.getTrajets();
 
         List<Trajet> listeTrajets = new ArrayList<Trajet>();
-        if(listeAllTrajets != null) {
+        if (listeAllTrajets != null) {
             for (Trajet t : listeAllTrajets) {
-                if (t.getStatut().equals(typeHistoTrajet) && t.getConducteurIdUser() == idUser)
+                if (t.getStatut().equals(typeHistoTrajet)
+                        && t.getConducteurIdUser() == idUser)
                     listeTrajets.add(t);
             }
         }
         return listeTrajets;
     }
 
-   @Override
-   public List<Trajet> getHistoTrajetsAsPassengerByType(int idUser, String typeHistoTrajet) {
-       
-       String query = "SELECT u FROM Utilisateur u inner join fetch u.trajets WHERE u.idUtilisateur = :idUser";
-       Utilisateur u = (Utilisateur) em.createQuery(query).setParameter("idUser",idUser).getSingleResult();
+    @Override
+    public List<Trajet> getHistoTrajetsAsPassengerByType(int idUser,
+            String typeHistoTrajet) {
 
-       List<Trajet> listeAllTrajets = u.getTrajets();
+        String query = "SELECT u FROM Utilisateur u inner join fetch u.trajets WHERE u.idUtilisateur = :idUser";
+        Utilisateur u = (Utilisateur) em.createQuery(query)
+                .setParameter("idUser", idUser).getSingleResult();
 
-       List<Trajet> listeTrajets = new ArrayList<Trajet>();
-       if(listeAllTrajets != null) {
-           for (Trajet t : listeAllTrajets) {
-               if (t.getStatut().equals(typeHistoTrajet) && t.getConducteurIdUser() != idUser)
-                   listeTrajets.add(t);
-           }
-       }
-       return listeTrajets;
-   }
+        List<Trajet> listeAllTrajets = u.getTrajets();
 
-@Override
-public Trajet rechercheById(Integer idTrajet) {
+        List<Trajet> listeTrajets = new ArrayList<Trajet>();
+        if (listeAllTrajets != null) {
+            for (Trajet t : listeAllTrajets) {
+                if (t.getStatut().equals(typeHistoTrajet)
+                        && t.getConducteurIdUser() != idUser)
+                    listeTrajets.add(t);
+            }
+        }
+        return listeTrajets;
+    }
 
-    System.out.println("==============================================");
-    System.out.println("========== A ================");
-    System.out.println("==============================================");
+    @Override
+    public Trajet rechercheById(Integer idTrajet) {
 
-//    Trajet trajet = new Trajet();
-//    trajet = em.find(Trajet.class, trajet.getIdTrajet());
+        System.out.println("==============================================");
+        System.out.println("========== A ================");
+        System.out.println("==============================================");
 
-    
-    Trajet ttt = em.find(Trajet.class, idTrajet);
+        // Trajet trajet = new Trajet();
+        // trajet = em.find(Trajet.class, trajet.getIdTrajet());
 
-    
-    System.out.println("==============================================");
-    System.out.println("========== B ================");
-    System.out.println("==============================================");
+        Trajet ttt = em.find(Trajet.class, idTrajet);
 
-    if (ttt != null)
-    System.out.println("=====> Trajet : " + ttt.getNbPassagersMax());
-    else
-        System.out.println("===== ttt est nul =========");
+        System.out.println("==============================================");
+        System.out.println("========== B ================");
+        System.out.println("==============================================");
 
-    //System.exit(0);
-    //Utilisateur user = em.find(Utilisateur.class, idUser);
-    
-//    String requete = "SELECT trajet FROM Trajet trajet WHERE trajet.id_trajet = :idTrajet";
-//    List<Trajet> liste = em.createQuery(requete, Utilisateur.class)
-//                                .setParameter("emailUser", email)
-//                                .setParameter("passwordUser", password)
-//                                .getResultList();
-//    
-//    String requete = "SELECT user FROM Utilisateur user WHERE user.email = :emailUser and user.password = :passwordUser";
-//    List<Utilisateur> liste = em.createQuery(requete, Utilisateur.class)
-//                                .setParameter("emailUser", email)
-//                                .setParameter("passwordUser", password)
-//                                .getResultList();
-//    
-    
-    
-//    Trajet tt = em.find(Trajet.class, idTrajet);
-//    
-//    System.out.println("TRAJET DANS RECHERCHE BY ID " + tt);
-    
-    return ttt;
-}
+        if (ttt != null)
+            System.out.println("=====> Trajet : " + ttt.getNbPassagersMax());
+        else
+            System.out.println("===== ttt est nul =========");
+
+        // System.exit(0);
+        // Utilisateur user = em.find(Utilisateur.class, idUser);
+
+        // String requete =
+        // "SELECT trajet FROM Trajet trajet WHERE trajet.id_trajet = :idTrajet";
+        // List<Trajet> liste = em.createQuery(requete, Utilisateur.class)
+        // .setParameter("emailUser", email)
+        // .setParameter("passwordUser", password)
+        // .getResultList();
+        //
+        // String requete =
+        // "SELECT user FROM Utilisateur user WHERE user.email = :emailUser and user.password = :passwordUser";
+        // List<Utilisateur> liste = em.createQuery(requete, Utilisateur.class)
+        // .setParameter("emailUser", email)
+        // .setParameter("passwordUser", password)
+        // .getResultList();
+        //
+
+        // Trajet tt = em.find(Trajet.class, idTrajet);
+        //
+        // System.out.println("TRAJET DANS RECHERCHE BY ID " + tt);
+
+        return ttt;
+    }
 
 }
