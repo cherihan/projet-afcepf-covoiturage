f489e7d163abcda2387d9ae60b64108ad50a5af6
ryadh.afcepf@gmail.com <ryadh.afcepf@gmail.com@52cbff0f-ab16-f77c-8b75-df5b4e13430b>
2014-08-31 17:23:24 +0000

    MAJ

--- trunk/ProjetAtod21/CoVoiturage_Application/CoVoiturage_business/src/main/java/fr/afcepf/atod21/coVoiturage/businessImpl/BusinessRechercheImpl.java
+++ trunk/ProjetAtod21/CoVoiturage_Application/CoVoiturage_business/src/main/java/fr/afcepf/atod21/coVoiturage/businessImpl/BusinessRechercheImpl.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -35,27 +36,83 @@ public class BusinessRechercheImpl implements IBusinessRecherche {
     }
 
     @Override
-    public List<TrajetDto> rechercherTrajetSOAP(Date paramDateDepart, String paramVilleDepart, String paramVilleArrivee) {
-        List<Trajet> listeTrajets = new ArrayList<Trajet>();
-        List<TrajetDto> listeTrajetsDto = null;
+    public List<TrajetDto> rechercherTrajetDto(Date paramDateDepart, String paramVilleDepart, String paramVilleArrivee) {
+        List<Trajet> listeTrajets = daoRecherche.rechercherTrajet(paramDateDepart, paramVilleDepart, paramVilleArrivee);
+
+        if (listeTrajets == null)
+            return null;
         
-        listeTrajets = daoRecherche.rechercherTrajet(paramDateDepart, paramVilleDepart, paramVilleArrivee);
+        List<TrajetDto> listeTrajetsDto = new ArrayList<TrajetDto>();
         
-        //TODO remplissage de listeTrajetsDto
-    
+        TrajetDto trajetDto = null;
+        
+        for (Trajet trajet : listeTrajets) {
+            
+            trajetDto = new TrajetDto();
+            BeanUtils.copyProperties(trajet, trajetDto);
+            trajetDto.setDateCreationDto(trajet.getDateCreation().toString());
+            trajetDto.setDateDepartDto(trajet.getDateDepart().toString());
+            trajetDto.setVilleDepartDto(trajet.getVilleDepart().getNom());
+            trajetDto.setVilleArriveeDto(trajet.getVilleArrivee().getNom());
+            
+            /*
+            List<AvisDto> listeAvisDto = new ArrayList<AvisDto>();
+            AvisDto avisDto = new AvisDto();
+            List<Avis> listeAvis = trajet.getAvis();
+
+            if (listeAvis != null) {
+                for (Avis avis : trajet.getAvis()) {
+                    BeanUtils.copyProperties(avis, avisDto);
+                    listeAvisDto.add(avisDto);
+                }
+
+                trajetDto.setListeAvisDto(listeAvisDto);
+            }
+            */
+            
+            /*
+            List<CommentaireTrajetDto> listeCommentaireTrajetDto = new ArrayList<CommentaireTrajetDto>();
+            CommentaireTrajetDto commentaireTrajetDto = new CommentaireTrajetDto();
+            if (listeCommentaireTrajetDto != null) {
+                for (CommentaireTrajet commentaireTrajet : trajet.getCommentaireTrajets()) {
+                    BeanUtils.copyProperties(commentaireTrajet, commentaireTrajetDto);
+                    listeCommentaireTrajetDto.add(commentaireTrajetDto);
+                }
+
+                trajetDto.setListeCommentairesDto(listeCommentaireTrajetDto);
+            }
+            */
+
+            listeTrajetsDto.add(trajetDto);
+        }
+
         return listeTrajetsDto;
     }
 
     @Override
-    public List<TrajetDto> rechercherTrajetParVilleDepartSOAP(Date paramDateDepart, String paramVilleDepart) {
-        List<Trajet> listeTrajets = new ArrayList<Trajet>();
-        List<TrajetDto> listeTrajetsDto = null;
+    public List<TrajetDto> rechercherTrajetParVilleDepartDto(Date paramDateDepart, String paramVilleDepart) {
         
-        listeTrajets = daoRecherche.rechercherTrajetParVilleDepart(paramDateDepart, paramVilleDepart);
+         List<Trajet> listeTrajets = daoRecherche.rechercherTrajetParVilleDepart(paramDateDepart, paramVilleDepart);
 
-        //TODO remplissage de listeTrajetsDto
+        if (listeTrajets == null)
+            return null;
+        
+        List<TrajetDto> listeTrajetsDto = new ArrayList<TrajetDto>();
+        
+        TrajetDto trajetDto = null;
+        
+        for (Trajet trajet : listeTrajets) {
+            
+            trajetDto = new TrajetDto();
+            BeanUtils.copyProperties(trajet, trajetDto);
+            trajetDto.setDateCreationDto(trajet.getDateCreation().toString());
+            trajetDto.setDateDepartDto(trajet.getDateDepart().toString());
+            trajetDto.setVilleDepartDto(trajet.getVilleDepart().getNom());
+            trajetDto.setVilleArriveeDto(trajet.getVilleArrivee().getNom());
+            listeTrajetsDto.add(trajetDto);
+        }
 
-        return listeTrajetsDto;
+            return listeTrajetsDto;
     }
 
 
