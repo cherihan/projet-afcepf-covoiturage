3ddeb32ceca0f2f3a1fe3f2a8105fcb8ba1a6b09
ryadh.afcepf@gmail.com <ryadh.afcepf@gmail.com@52cbff0f-ab16-f77c-8b75-df5b4e13430b>
2014-08-31 17:24:19 +0000

    MAJ

--- trunk/ProjetAtod21/CoVoiturage_Application/CoVoiturage_data/src/main/java/fr/afcepf/atod21/coVoiturage/dao/IDaoTrajet.java
+++ trunk/ProjetAtod21/CoVoiturage_Application/CoVoiturage_data/src/main/java/fr/afcepf/atod21/coVoiturage/dao/IDaoTrajet.java
@@ -9,14 +9,12 @@ import fr.afcepf.atod21.coVoiturage.entity.Ville;
 public interface IDaoTrajet {
 
 	void ajouterCommentaireTrajet(int idTrajet, int idUser);
-	//Trajet creerTrajet(int idUser, String villeDepart, String villeArrivee, Date date, int tarif, int nbPassagers);
 	Integer creerTrajet(Trajet trajet, Utilisateur user);
-	void supprimerTrajet(int idTrajet, int idUser);
+	boolean supprimerTrajet(Trajet trajet, Utilisateur user);
 	List<Ville> getAllVilles();
 	List<Trajet> getAllHistoTrajets(int idUser);
-    List<Trajet> getHistoTrajetsAsConductorByType(int idUser, String typeHistoTrajet);
-    List<Trajet> getHistoTrajetsAsPassengerByType(int idUser, String typeHistoTrajet);
-
+    List<Trajet> getHistoTrajetsByType(int idUser, String typeHistoTrajet);
+    
     boolean sInscrireTrajet(Trajet trajet, Utilisateur user);
     boolean seDesinscrireTrajet(Integer idTrajet, Integer idUser);
     boolean annulerInscriptionTrajet(Integer idTrajet, Integer idUser);
