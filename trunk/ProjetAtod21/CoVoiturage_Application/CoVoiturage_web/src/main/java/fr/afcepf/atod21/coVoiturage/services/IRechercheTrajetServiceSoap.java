eaa3f8cf683bfc2d2766300de5b31d3c9d4445a8
ryadh.afcepf@gmail.com <ryadh.afcepf@gmail.com@52cbff0f-ab16-f77c-8b75-df5b4e13430b>
2014-08-31 16:22:42 +0000

    MAJ

--- trunk/ProjetAtod21/CoVoiturage_Application/CoVoiturage_web/src/main/java/fr/afcepf/atod21/coVoiturage/services/IRechercheTrajetServiceSoap.java
+++ trunk/ProjetAtod21/CoVoiturage_Application/CoVoiturage_web/src/main/java/fr/afcepf/atod21/coVoiturage/services/IRechercheTrajetServiceSoap.java
@@ -3,15 +3,13 @@ package fr.afcepf.atod21.coVoiturage.services;
 import java.util.Date;
 import java.util.List;
 
-import fr.afcepf.atod21.coVoiturage.business.dto.TrajetDto;
-import fr.afcepf.atod21.coVoiturage.entity.Trajet;
-
 import javax.jws.WebService;
+
+import fr.afcepf.atod21.coVoiturage.business.dto.TrajetDto;
 
 @WebService
 public interface IRechercheTrajetServiceSoap {
 
     List<TrajetDto> rechercherTrajetSOAP(Date dateDepart, String villeDepart, String villeArrivee);
-    List<TrajetDto> rechercherTrajetParVilleDepartSOAP(Date dateDepart, String villeDepart);
 
 }
