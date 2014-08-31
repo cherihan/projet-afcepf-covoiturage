5eda154dd33c190989c9b8ff4e6da93f713af0ff
ryadh.afcepf@gmail.com <ryadh.afcepf@gmail.com@52cbff0f-ab16-f77c-8b75-df5b4e13430b>
2014-08-31 16:08:13 +0000

    MAJ

--- ProjetAtod21/CoVoiturage_Application/CoVoiturage_business/src/main/java/fr/afcepf/atod21/coVoiturage/business/IBusinessRecherche.java
+++ ProjetAtod21/CoVoiturage_Application/CoVoiturage_business/src/main/java/fr/afcepf/atod21/coVoiturage/business/IBusinessRecherche.java
@@ -9,8 +9,8 @@ import fr.afcepf.atod21.coVoiturage.entity.Trajet;
 public interface IBusinessRecherche {
 
     List<Trajet> rechercherTrajet(Date dateDepart, String villeDepart, String villeArrivee);
-    List<TrajetDto> rechercherTrajetSOAP(Date dateDepart, String villeDepart, String villeArrivee);
+    List<TrajetDto> rechercherTrajetDto(Date dateDepart, String villeDepart, String villeArrivee);
     List<Trajet> rechercherTrajetParVilleDepart(Date dateDepart, String villeDepart);
-    List<TrajetDto> rechercherTrajetParVilleDepartSOAP(Date dateDepart, String villeDepart);
+    List<TrajetDto> rechercherTrajetParVilleDepartDto(Date dateDepart, String villeDepart);
 
 }
