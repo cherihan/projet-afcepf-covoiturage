ca78aad493cb26f8983c1a2677be4d51f5f907bb
elmehdi.elbrini@gmail.com <elmehdi.elbrini@gmail.com@52cbff0f-ab16-f77c-8b75-df5b4e13430b>
2014-09-17 09:35:03 +0000

    Complete logic of actionServlet

--- ProjetFramework/cov/src/main/java/fr/afcepf/atod21/cov/bean/BeanCovPopulate.java
+++ ProjetFramework/cov/src/main/java/fr/afcepf/atod21/cov/bean/BeanCovPopulate.java
@@ -4,37 +4,34 @@ import java.lang.reflect.Method;
 import java.util.Map;
 
 public class BeanCovPopulate {
-    private String foo;
-    public String getFoo() {
-        return foo;
-    }
-    public void setFoo(String paramFoo) {
-        foo = paramFoo;
-    }
+	public Object populateBean(Object obj, Map<String, String[]> requestParamsMap) {
 
-    public Object populateBean(Object obj, Map<String, String[]> requestParamsMap){
 		Class dynamicClass = obj.getClass();
 		Object objectClass = null;
+
 		try {
-            objectClass = dynamicClass.newInstance();
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.out.println(e);
-        }
+			objectClass = dynamicClass.newInstance();
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println(e);
+		}
+
 		for (String currentKey : requestParamsMap.keySet()) {
-            Method[] methodList = dynamicClass.getMethods();
-            
-            for (Method currentMethod : methodList) {
-                if (currentMethod.getName().compareToIgnoreCase("set" + currentKey) == 0) {
-                    try {
-                        currentMethod.invoke(objectClass, requestParamsMap.get(currentKey));
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                        System.out.println(e);
-                    }
-                }
-            }
-        }
+			try 
+			{
+				Method[] methodList = dynamicClass.getMethods();
+
+				for (Method currentMethod : methodList) {
+					if (currentMethod.getName().compareToIgnoreCase(
+							"set" + currentKey) == 0) {
+						currentMethod.invoke(objectClass, requestParamsMap.get(currentKey));
+					}
+				}
+				
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
 		return null;
 	}
 }
