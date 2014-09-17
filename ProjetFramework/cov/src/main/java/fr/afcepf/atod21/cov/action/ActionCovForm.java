f16a58479c50354d15b1a2ca5329522d16ef8b52
benjaminvidon@gmail.com <benjaminvidon@gmail.com@52cbff0f-ab16-f77c-8b75-df5b4e13430b>
2014-09-17 07:32:58 +0000

    Développement de la méthode Validate

--- ProjetFramework/cov/src/main/java/fr/afcepf/atod21/cov/action/ActionCovForm.java
+++ ProjetFramework/cov/src/main/java/fr/afcepf/atod21/cov/action/ActionCovForm.java
@@ -1,5 +1,9 @@
 package fr.afcepf.atod21.cov.action;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 import javax.servlet.ServletRequest;
 
 import fr.afcepf.atod21.cov.errors.ActionCovErrors;
@@ -8,10 +12,31 @@ import fr.afcepf.atod21.cov.mapping.ActionCovMapping;
 public class ActionCovForm implements IActionCovForm {
 
 	@Override
-	public ActionCovErrors validate(ActionCovMapping mapping,
-			ServletRequest request) {
-		// TODO Auto-generated method stub
-		return null;
+	public ActionCovErrors validate(ActionCovMapping mapping, ServletRequest request) {
+	    
+	    List<String> attributesList = new ArrayList<>(); //Contient la liste de l'ensemble des attributs à récupérer dans la requête.
+	    Boolean allAttributesCorrect = true; // Devient false si un champ n'a pas été rempli par l'utilisateur.
+	    
+	    attributesList = (List<String>) request.getAttributeNames(); //On recupère l'ensemble des noms des attributs de la requête.
+	    
+	    for (String currentAttribute : attributesList) {
+            if (request.getAttribute(currentAttribute).equals(null)
+                    || request.getAttribute(currentAttribute).equals("")){
+                System.out.println("Erreur dans le remplissage des champs : le champ " +
+                                    currentAttribute + " est vide !");
+                allAttributesCorrect = false;
+            }
+        }
+	    if (allAttributesCorrect = true){
+	        return null;
+	    } else {
+	        ActionCovErrors champNonRempli = new ActionCovErrors();
+            return champNonRempli;
+	    }
+	}
+	
+	public void peuplement (ActionCovForm actionCovForm, Map<String, String[]> requestMapAttributes){
+	    
 	}
 
 }
